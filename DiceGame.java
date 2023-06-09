b7b1bdfe254e4e7118d77822b747083d24a4d4dc
muizu <muizu0920555@protonmail.com>
2023-06-09 12:34:43 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -10,7 +10,13 @@ public class DiceGame {
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + total);
+
+        if (total > 7) {
+            System.out.println("You won!");
+        }
     }
+
+    
     
     public static int rollDice() {
         Random random = new Random();
